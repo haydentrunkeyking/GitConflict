cac447f9584ccb15c4498cd66a8c44c869420223
Hayden Trunkeyking <hdtrunkeyking@nic.edu>
2025-10-27 10:46:37 -0700

    not funny anymoe

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+void say(string s);
 
 int main()
 {
@@ -8,6 +9,9 @@ int main()
 
     cout << "Hello World!\n";
 
-
     return 0;
+}
+void say(string s)
+{
+    cout << s << endl;
 }
